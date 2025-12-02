0809ca63496f1da14b070100f9d2177b7f21def5
bberny2016 <bberny2016@gmail.com>
2025-12-02 00:05:54 -0600

    cicd pipeline

---
+++ .github/workflows/test.py
@@ -0,0 +1,11 @@
+from app import app  # type: ignore
+
+def test_app_exists():
+    """Test that the app exists"""
+    assert app is not None
+
+def test_home_page():
+    """Test the home page endpoint"""
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
